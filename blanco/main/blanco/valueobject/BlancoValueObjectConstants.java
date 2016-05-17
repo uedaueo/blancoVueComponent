2d0004e1c10290d40c9ef3baf7cfa864aa185fab
Saki Matsumoto <kinoko@ueo.co.jp>
2016-05-17 19:08:28 +0900

    .xlsxに対応

--- blanco/main/blanco/valueobject/BlancoValueObjectConstants.java
+++ blanco/main/blanco/valueobject/BlancoValueObjectConstants.java
@@ -23,7 +23,7 @@ public class BlancoValueObjectConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "1.2.4";
+    public static final String VERSION = "2.0.1";
 
     /**
      * 項目番号:4<br>
