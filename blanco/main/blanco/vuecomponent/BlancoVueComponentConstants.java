263748325adff4c16affc843bb3b7f14d2ab66d1
tueda <ueo@ueo.co.jp>
2020-10-07 14:16:08 +0900

    [POI4] 0.1.0: Now ready to use apache poi 4.1.2.

--- blanco/main/blanco/vuecomponent/BlancoVueComponentConstants.java
+++ blanco/main/blanco/vuecomponent/BlancoVueComponentConstants.java
@@ -20,7 +20,7 @@ public class BlancoVueComponentConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.0";
 
     /**
      * 項目番号:4<br>
